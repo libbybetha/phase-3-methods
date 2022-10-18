06229e3f490f14e1f768e28dc53f650b2f740dd2
Beth Njuguna <libbybetha@gmail.com>
2022-10-18 11:23:16 +0300

    completed assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,41 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end 
+greet
+
+greet "Jimmy"
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+    
+end
+greet_with_default
+
+
+
+
+def add(num1=2,num2=5)
+    return num1+num2
+end
+add
+
+
+def halve(number)
+    puts number / 2
+end 
+
+
+def halve(number=6)
+    if number.class !=Integer      
+            return nil
+    end
+    number / 2
+end
+    
